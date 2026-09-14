4f9716c4895cdd8fba6af0e9d149e10856b3e165
chandan902640-lab <chandan902640@gmail.com>
2026-09-14 15:36:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ def keep_alive():
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.event
 async def on_ready():
